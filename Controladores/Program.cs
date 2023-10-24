aa60d21516af25569f6bb3572b54b97b42878b96
DESKTOP-C3B8621\ruben <csi22-rpaccia@colegioaltair.net>
2023-10-24 11:45:11 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,24 @@
+﻿using ParOImpar.Servicios;
+
+namespace CuadradoNumero
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n1;
+
+            MenuInterfaz mi=new MenuImplemetacion();
+            n1 = mi.pedirNum();
+
+            if(n1%2==0 )
+            {
+                Console.WriteLine("El numero " + n1 + " es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + n1 + " es impar");
+            }
+        }
+    }
+}
---
+++ Servicios/MenuImplemetacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParOImpar.Servicios
+{
+    internal class MenuImplemetacion:MenuInterfaz
+    {
+        public int pedirNum()
+        {        
+            int n1;
+            Console.WriteLine("Introduzca un numero: ");
+            n1=Convert.ToInt32(Console.ReadLine());
+            return n1;
+       
+        }
+
+    }
+}
